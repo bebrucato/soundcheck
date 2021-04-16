b958bc11054cd2bbb150e4a5a7fba0ad8947ab77
B Brucato <bebrucato@gmail.com>
2021-04-15 17:39:33 -0700

    api routes/indexes complete

---
+++ soundcheck/routes/api/comments.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const commentController = require("../../controllers/commentController");
+
+router.route("/")
+  .get(commentController.findAll)
+  .post(commentController.create);
+
+router
+  .route("/:id")
+  .get(commentController.findById)
+  .put(commentController.update)
+  .delete(commentController.remove);
+
+module.exports = router;
