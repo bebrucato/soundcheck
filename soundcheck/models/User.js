dc9df5e4ae9e0d08b44a0a72d6ba52d88dd75844
B Brucato <bebrucato@gmail.com>
2021-04-16 11:01:41 -0700

    added bcrypt function/got rid of individual/band

--- soundcheck/models/User.js
+++ soundcheck/models/User.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 
 const userSchema = new Schema(
   {
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  individual: { type: Boolean },
-  band: { type: Boolean },
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
@@ -29,6 +28,22 @@ comments: [
   timestamps: true,
 }
 );
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  if (!user.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, (hashErr, hash) => {
+      if (hashErr) return next(hashErr);  
+      user.password = hash;
+      next();
+    });
+  });
+});
 
 const User = mongoose.model("User", userSchema);
 
