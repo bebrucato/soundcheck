3ee23c1ad1b3fac2ed71f6a666540992abcd6d6f
B Brucato <bebrucato@gmail.com>
2021-04-20 10:42:24 -0700

    added the chat widget component

--- soundcheck/client/src/App.js
+++ soundcheck/client/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import LoginCard from "./components/LoginCard/login-card";
 import SideBar from "./components/SideBar/side-bar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ChatWidget from "./components/ChatWidget/ChatWidget";
+import 'react-chat-widget/lib/styles.css';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <div>
         <LoginCard />
         <SideBar /> 
+        <ChatWidget />
+
          {/* <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/saved" component={Saved} />
