9474cd7032342e76bad43085c57dc3780e7614bb
Steven Moseni <smoseni@hotmail.com>
2021-04-20 22:39:59 -0700

    worked on axios calls and react pages

---
+++ soundcheck/client/src/pages/Signin/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import LoginCard from "./components/LoginCard/login-card";
+class Signin extends Component {
+    render() {
+        return (
+            <div>
+         <LoginCard/>
+            </div>
+
+            
+
+        )
+    }
+}
+
+export default Signin;
